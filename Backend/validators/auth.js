eb315f2d16139832ca91337becf177435a159ec4
James Hagood <cardfaux@tutanota.com>
2019-10-23 16:15:48 -0500

    added password reset on the backend

--- Backend/validators/auth.js
+++ Backend/validators/auth.js
@@ -4,20 +4,36 @@ exports.userSignupValidator = [
     check('name')
         .not()
         .isEmpty()
-        .withMessage('Name Is Required'),
+        .withMessage('Name is required'),
     check('email')
         .isEmail()
-        .withMessage('Must Be A Valid Email'),
+        .withMessage('Must be a valid email address'),
     check('password')
-        .isLength({min: 6})
-        .withMessage('Password Must Be At Least 6 Characters')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
 ];
 
 exports.userSigninValidator = [
     check('email')
         .isEmail()
-        .withMessage('Must Be A Valid Email'),
+        .withMessage('Must be a valid email address'),
     check('password')
-        .isLength({min: 6})
-        .withMessage('Password Must Be At Least 6 Characters')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
+];
+
+exports.forgotPasswordValidator = [
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+        .withMessage('Must be a valid email address')
+];
+
+exports.resetPasswordValidator = [
+    check('newPassword')
+        .not()
+        .isEmpty()
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
 ];
